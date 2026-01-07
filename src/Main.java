ed712335aac272b07b2f3961b8e17710600bb335
VandanaPapondiya <vandanadhakad84@gmail.com>
2026-01-08 02:34:31 +0530

    V1 and V2

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        System.out.printf("Vandana line8");
+        System.out.printf("Vandana1");
         for (int i = 1; i <= 20; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
-        System.out.printf("Vandana line14");
+        System.out.printf("Vandana2");
     }
 }
